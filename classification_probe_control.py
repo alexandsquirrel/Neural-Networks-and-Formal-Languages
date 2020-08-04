505426831d3adcbe27bba8ee7fa17778fc85f5e0
alexandsquirrel <fangsc@uw.edu>
2020-08-03 22:12:14 -0700

    add binary classification control test

--- classification_probe_control.py
+++ classification_probe_control.py
@@ -11,22 +11,22 @@ Hypothesis #1: does the model keeps a binary flag (3 / not 3) ?
 '''
 classification_probe = torch.nn.Linear(hidden_size, 2)
 
-def exactly_three_ABs(seq):
+def exactly_twenty_ABs(seq):
     # TODO: perhaps, more pythonic?
     count = 0
     AB = torch.Tensor([1, 0, 0, 0])
     for vec in seq:
         if torch.equal(vec, AB):
             count += 1
-    if count == 3:
+    if count == 20:
         return 0
     else:
         return 1
 
-training_inputs = make_dataset(exactly_three, quant_mapping, 300, 300)
-training_dataset = extract_hidden_states_and_labels(exactly_three_decider, training_inputs, exactly_three_ABs)
-validation_inputs = make_dataset(exactly_three, quant_mapping, 100, 100)
-validation_dataset = extract_hidden_states_and_labels(exactly_three_decider, validation_inputs, exactly_three_ABs)
+training_inputs = make_dataset(exactly_twenty, quant_mapping, 300, 300)
+training_dataset = extract_hidden_states_and_labels(exactly_three_decider, training_inputs, exactly_twenty_ABs)
+validation_inputs = make_dataset(exactly_twenty, quant_mapping, 100, 100)
+validation_dataset = extract_hidden_states_and_labels(exactly_three_decider, validation_inputs, exactly_twenty_ABs)
 
 def h1_loss_function(x, y):
     cross_entropy_loss_function = loss_function=torch.nn.CrossEntropyLoss(reduction='mean')
--- quantifiers.py
+++ quantifiers.py
@@ -14,3 +14,16 @@ nall = RegularLanguage(name="not_all", chars=quant_chars, max_length=20,
 
 exactly_three = RegularLanguage(name="exactly_three", chars=quant_chars, max_length=20,
                                 regex='[1-3]*0[1-3]*0[1-3]*0[1-3]*')
+
+def exactly_n(n):
+    name = 'exactly_' + str(n)
+    regex = '[1-3]*' + n * '0[1-3]*'
+    return RegularLanguage(name=name, chars=quant_chars, max_length=20, regex=regex)
+
+exactly_zero = exactly_n(0)
+exactly_one = exactly_n(1)
+exactly_two = exactly_n(2)
+exactly_four = exactly_n(4)
+exactly_five = exactly_n(5)
+exactly_eight = exactly_n(8)
+exactly_twenty = exactly_n(20)
