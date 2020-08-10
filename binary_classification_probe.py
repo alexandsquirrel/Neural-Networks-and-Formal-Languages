32d3abb7a796fff565a9d9c6b96dd650c4337af7
alexandsquirrel <fangsc@uw.edu>
2020-08-09 18:29:18 -0700

    clean up redundant probes

--- binary_classification_probe.py
+++ binary_classification_probe.py
@@ -8,30 +8,31 @@ exactly_three_decider = Decider(input_dimension=4, hidden_size=12)
 exactly_three_decider.load_state_dict(torch.load('ckpt/best.pt'))
 
 '''
-Hypothesis #1: does the model do a multi-way classification?
-zero - one - two - three - more_than_three
-Note: this is by chance also a DFA probe, on this task.
-
-YET ANOTHER CONTROL TEST FOR ABOVE.
-Note: we somehow expect n < 3 to converge nicely, for the clustering argument.
+This probe tests whether we can successfully extract the information of 
+"exactly n" in the hidden representation of the model of exactly_3.
+For n = 3, we expect the probe to converge nicely.
+For n != 3, we expect the probe not to converge.
 '''
 
-def run_multi_classification_probe(n, verbose=True):
-    classification_probe = torch.nn.Linear(hidden_size, n + 2)
+def run_binary_classification_probe(n, verbose=True):
+    classification_probe = torch.nn.Linear(hidden_size, 2)
 
-    def num_AB(seq):
+    def exactly_n_ABs(seq):
         # TODO: perhaps, more pythonic?
         count = 0
         AB = torch.Tensor([1, 0, 0, 0])
         for vec in seq:
             if torch.equal(vec, AB):
                 count += 1
-        return min(count, n + 1)
+        if count == n:
+            return 0
+        else:
+            return 1
 
     training_inputs = make_dataset(exactly_n(n), quant_mapping, 1000, 1000)
-    training_dataset = extract_hidden_states_and_labels(exactly_three_decider, training_inputs, num_AB)
-    validation_inputs = make_dataset(exactly_n(n), quant_mapping, 100, 100)
-    validation_dataset = extract_hidden_states_and_labels(exactly_three_decider, validation_inputs, num_AB)
+    training_dataset = extract_hidden_states_and_labels(exactly_three_decider, training_inputs, exactly_n_ABs)
+    validation_inputs = make_dataset(exactly_n(n), quant_mapping, 200, 200)
+    validation_dataset = extract_hidden_states_and_labels(exactly_three_decider, validation_inputs, exactly_n_ABs)
 
     def h1_loss_function(x, y):
         cross_entropy_loss_function = torch.nn.CrossEntropyLoss(reduction='mean')
@@ -40,17 +41,13 @@ def run_multi_classification_probe(n, verbose=True):
     train_a_probe(probe=classification_probe,
                 data_training=training_dataset,
                 data_validation=validation_dataset,
-                num_epochs=20,
+                num_epochs=10,
                 loss_function=h1_loss_function)
 
-    '''
-    Apparently the probe is not gonna converge nicely.
-    We are also interested in the mistakes that it makes: are they for 
-    '''
     if verbose:
         print_eval_results(classification_probe, validation_dataset)
 
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    run_multi_classification_probe(n)
+    run_binary_classification_probe(n, verbose=False)
--- multi_classification_probe.py
+++ multi_classification_probe.py
@@ -1,6 +1,7 @@
 from probe_utils import *
 from dataset import *
 from quantifiers import *
+import sys
 
 hidden_size = 12
 exactly_three_decider = Decider(input_dimension=4, hidden_size=12)
@@ -10,31 +11,49 @@ exactly_three_decider.load_state_dict(torch.load('ckpt/best.pt'))
 Hypothesis #1: does the model do a multi-way classification?
 zero - one - two - three - more_than_three
 Note: this is by chance also a DFA probe, on this task.
+
+Note: we somehow expect n < 3 to converge nicely, for the clustering argument.
+For n = 3, it should converge without question.
+For n > 3, we expect the probe to diverge.
+
+Usage: supply n as a command-line argument.
 '''
-classification_probe = torch.nn.Linear(hidden_size, 5)
 
-def num_AB(seq):
-    # TODO: perhaps, more pythonic?
-    count = 0
-    AB = torch.Tensor([1, 0, 0, 0])
-    for vec in seq:
-        if torch.equal(vec, AB):
-            count += 1
-    return min(count, 4)
+def run_multi_classification_probe(n, verbose=True):
+    classification_probe = torch.nn.Linear(hidden_size, n + 2)
 
-training_inputs = make_dataset(exactly_three, quant_mapping, 1000, 1000)
-training_dataset = extract_hidden_states_and_labels(exactly_three_decider, training_inputs, num_AB)
-validation_inputs = make_dataset(exactly_three, quant_mapping, 100, 100)
-validation_dataset = extract_hidden_states_and_labels(exactly_three_decider, validation_inputs, num_AB)
+    def num_AB(seq):
+        # TODO: perhaps, more pythonic?
+        count = 0
+        AB = torch.Tensor([1, 0, 0, 0])
+        for vec in seq:
+            if torch.equal(vec, AB):
+                count += 1
+        return min(count, n + 1)
 
-def h1_loss_function(x, y):
-    cross_entropy_loss_function = torch.nn.CrossEntropyLoss(reduction='mean')
-    return cross_entropy_loss_function(x, y)
+    training_inputs = make_dataset(exactly_n(n), quant_mapping, 1000, 1000)
+    training_dataset = extract_hidden_states_and_labels(exactly_three_decider, training_inputs, num_AB)
+    validation_inputs = make_dataset(exactly_n(n), quant_mapping, 100, 100)
+    validation_dataset = extract_hidden_states_and_labels(exactly_three_decider, validation_inputs, num_AB)
 
-train_a_probe(probe=classification_probe,
-              data_training=training_dataset,
-              data_validation=validation_dataset,
-              num_epochs=40,
-              loss_function=h1_loss_function)
+    def h1_loss_function(x, y):
+        cross_entropy_loss_function = torch.nn.CrossEntropyLoss(reduction='mean')
+        return cross_entropy_loss_function(x, y)
 
-print_eval_results(classification_probe, validation_dataset)
+    train_a_probe(probe=classification_probe,
+                data_training=training_dataset,
+                data_validation=validation_dataset,
+                num_epochs=20,
+                loss_function=h1_loss_function)
+
+    '''
+    When the probe does not converge nicely, we are interested in the kind
+    of mistake it makes.
+    '''
+    if verbose:
+        print_eval_results(classification_probe, validation_dataset)
+
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    run_multi_classification_probe(n)
